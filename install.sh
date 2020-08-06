b21a2d2879061a080e5e45e5433316d4695959e1
Timm Bangma <bangma@hp.com>
2020-08-06 19:39:21 -0400

    mv appimage to usr local bin.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt install ./ripgrep-12.1.1.deb -y
 
 echo 'installing neovim'
 curl -sSLo nvim.appimage https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
-chmod +x ./nvim.appimage && ./nvim.appimage
+chmod +x ./nvim.appimage && sudo mv ./nvim.appimage /usr/local/bin/nvim
 
 echo 'installing init.vim configuration file'
 cp ./init.vim ~/.config/nvim/init.vim -f
