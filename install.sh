17b482da28d3be559c89db7ac932cbbe94ac66e3
Ubuntu <ubuntu@ip-172-31-63-117.us-west-2.compute.internal>
2020-08-06 23:49:32 +0000

    remove artifacts, add +x on install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo 'installing ripgrep'
 curl -sSLo ripgrep-12.1.1.deb https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb
-sudo apt install ./ripgrep-12.1.1.deb -y
+sudo apt install ./ripgrep-12.1.1.deb -y && rm ripgrep-12.1.1.deb
 
 echo 'installing neovim'
 curl -sSLo nvim.appimage https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
@@ -13,8 +13,4 @@ echo 'installing vim-plug plugin manager'
 sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo 'installing nvim plugins'
-nvim +PlugInstall +qa &
-
-echo 'removing leftover artifact downloads'
-rm ./nvim.appimage
-rm ./ripgrep-12.1.1.deb
+nvim +PlugInstall
